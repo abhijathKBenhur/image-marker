6f719a022546e43640582269e57226830faaa8e2
abhijathbenhur <abhijath.benhur@tellius.com>
2022-11-14 23:28:45 +0530

    initial marker dependency

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import car from "../src/car.jpeg";
+import ImageMarker, { Marker, MarkerComponentProps } from "react-image-marker";
+import Dialog from 'react-bootstrap-dialog'
+
+
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [markers, setMarkers] = useState([]);
+  const [image, setImage] = useState(null);
+  const CustomMarker = (props) => {
+    return (
+      <>
+        <p
+          className="custom-marker"
+          style={{ color: "black", backgroundColor: "red" }}
+        >
+          My custom marker - {props.itemNumber}
+        </p>
+      </>
+    );
+  };
+
+  const addMarker = (marker) => {
+    
+    Dialog.TextPrompt({initialValue: 'me@example.com', placeholder: 'your email'})
+    setMarkers([...markers, marker])
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ImageMarker
+          src={car}
+          markers={markers}
+          onAddMarker={(marker) => addMarker(marker)}
+          // markerComponent={CustomMarker}
+        />
       </header>
     </div>
   );
